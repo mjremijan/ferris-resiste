c5c26c5d0d20c2008284d889d891105a9c890151
Michael Remijan <mjremijan@yahoo.com>
2018-02-15 14:56:21 -0600

    commit

--- ferris-resiste-console/src/main/java/org/ferris/resiste/console/exception/UncaughtExceptionService.java
+++ ferris-resiste-console/src/main/java/org/ferris/resiste/console/exception/UncaughtExceptionService.java
@@ -1,10 +1,13 @@
 package org.ferris.resiste.console.exception;
 
 import java.lang.Thread.UncaughtExceptionHandler;
+import java.util.ArrayList;
+import java.util.Arrays;
 import javax.annotation.Priority;
 import javax.enterprise.event.Event;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
+import org.ferris.resiste.console.email.EmailErrorEvent;
 import org.ferris.resiste.console.exit.ExitEvent;
 import org.ferris.resiste.console.main.StartupEvent;
 import static org.ferris.resiste.console.main.StartupEvent.EXCEPTION;
@@ -28,6 +31,9 @@ public class UncaughtExceptionService implements UncaughtExceptionHandler {
     @Inject
     protected Event<ExitEvent> exitEvent;
 
+    @Inject
+    protected Event<EmailErrorEvent> error;
+
     public void observesStartup(
             @Observes @Priority(EXCEPTION) StartupEvent event
     ) {
@@ -39,6 +45,20 @@ public class UncaughtExceptionService implements UncaughtExceptionHandler {
 
     @Override
     public void uncaughtException(Thread t, Throwable e) {
+
+        // Remove default handler...infinte circle?
+        exceptionTool.setDefaultUncaughtExceptionHandler(null);
+
+        // Try to send error email
+        try {
+            error.fire(new EmailErrorEvent(
+                new ArrayList<String>(1){{
+                    add(String.format("Uncaught exception: %s", Arrays.toString(e.getStackTrace())));
+                }}
+            ));
+        } catch (Exception ignore){}
+
+        // View and exit
         uncaughtExceptionPage.view(e);
         exitEvent.fire(new ExitEvent());
     }
