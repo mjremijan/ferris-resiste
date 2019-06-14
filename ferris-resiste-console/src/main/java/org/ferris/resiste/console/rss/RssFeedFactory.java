2577c83bf1024af41c3843c4d0f52b4ea44e816e
Michael Remijan <mjremijan@yahoo.com>
2019-06-14 10:41:33 -0500

    Added a check of the HTTP response code. If it's not 200OK then I throw an exception with the code.

--- ferris-resiste-console/src/main/java/org/ferris/resiste/console/rss/RssFeedFactory.java
+++ ferris-resiste-console/src/main/java/org/ferris/resiste/console/rss/RssFeedFactory.java
@@ -8,7 +8,7 @@ import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.URLConnection;
+import java.net.HttpURLConnection;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
@@ -39,9 +39,30 @@ public class RssFeedFactory {
 
         try {
 
-            URLConnection connection = feedUrl.getUrl().openConnection();
+            HttpURLConnection connection = (HttpURLConnection)feedUrl.getUrl().openConnection();
             connection.setRequestProperty("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36");
+            connection.setRequestProperty("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3");
+            connection.setRequestProperty("accept-encoding", "deflate, br");
+            connection.setRequestProperty("accept-language", "en-US,en;q=0.9");
+            connection.setRequestProperty("upgrade-insecure-requests", "1");
             connection.connect();
+
+            if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                System.out.printf("Response code is not OK...it is: %d%n", connection.getResponseCode());
+                String errorString = "UNABLE TO OBTAIN";
+                try {
+                    errorString
+                        = new BufferedReader(new InputStreamReader(connection.getErrorStream(), "UTF-8")).lines().collect(Collectors.joining("\n"));
+                } catch (Exception ignore) {}
+                
+                throw new RuntimeException(
+                    String.format(
+                        "HTTP response is not OK. CODE=%d, ERROR_STRING=\"%s\""
+                        , connection.getResponseCode()
+                        , errorString
+                    )
+                );
+            }
 
             rawXml
                 = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8")).lines().collect(Collectors.joining("\n"));
