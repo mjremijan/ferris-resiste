ad6f248168e22a065b6ac2c4c48c48cc94598654
Michael Remijan <mjremijan@yahoo.com>
2024-03-15 15:41:26 -0500

    RESISTE-124 Work on getting the commented out unit tests working again.

--- ferris-resiste-console/src/test/java/org/ferris/resiste/console/rss/RssEntryTest.java
+++ ferris-resiste-console/src/test/java/org/ferris/resiste/console/rss/RssEntryTest.java
@@ -9,66 +9,72 @@ import org.junit.Test;
  */
 public class RssEntryTest {
 
-//    @Test
-//    public void test_setContents_removes_hieght_and_width() {
-//        String html 
-//            = "<p><a href=\"https://mars.nasa.gov/news/9549/\">\n" +
-//              "<img src=\"https://mars.nasa.gov/system/news_items/main_images/9549_PIA24925_MAIN.jpg\" \n   style=\"padding-right:10px; \n padding-bottom:5px;\" \n align=\"left\" \n alt=\"Read article: Team Assessing SHERLOC Instrument on NASA's Perseverance Rover\" \n width=\"100\" \n height=\"75\" \n border=\"0\" /></a><br /><img src='' height='5' foo='bar' width=9></img><br />Engineers are working to stabilize a dust cover on one of the science instrument’s cameras.</p><img src=\"f\"   width=''/><br clear=\"all\"/><img src='b' height=5></img><br /><img <";
-//
-//        String expected 
-//            = "<p><a href=\"https://mars.nasa.gov/news/9549/\">\n" +
-//              "<img src=\"https://mars.nasa.gov/system/news_items/main_images/9549_PIA24925_MAIN.jpg\" \n   style=\"padding-right:10px; \n padding-bottom:5px;\" \n align=\"left\" \n alt=\"Read article: Team Assessing SHERLOC Instrument on NASA's Perseverance Rover\" \n resistewidth=\"100\" \n resisteheight=\"75\" \n border=\"0\" /></a><br /><img src='' resisteheight='5' foo='bar' resistewidth=9></img><br />Engineers are working to stabilize a dust cover on one of the science instrument’s cameras.</p><img src=\"f\"   resistewidth=''/><br clear=\"all\"/><img src='b' resisteheight=5></img><br /><img <";
-//        
-//        RssEntry entry = new RssEntry();
-//        entry.setContents(html);
-//        Assert.assertEquals(expected, entry.getContents());
-//    }
-//
-//    @Test
-//    public void test_setContents_no_html() {
-//        String html, expected;
-//        html = expected =
-//            "plain text content with no html";
-//
-//        RssEntry entry = new RssEntry();
-//        entry.setContents(html);
-//        Assert.assertEquals(expected, entry.getContents());
-//    }
-//    
-//    @Test
-//    public void test_setContents_lots_of_lessthan_but_no_tags() {
-//        String html, expected;
-//        html = expected =
-//            "this < is < some<thin< that migh> be <<< difficult> to parse<?> ><?";
-//
-//        RssEntry entry = new RssEntry();
-//        entry.setContents(html);
-//        Assert.assertEquals(expected, entry.getContents());
-//    }
-//    
-//    @Test
-//    public void test_setContents_lots_of_img_tags_but_no_attributes() {
-//        String html, expected;
-//        html = expected =
-//            "this <img> tag and <img/> and <img /> and <img></img>";
-//
-//        RssEntry entry = new RssEntry();
-//        entry.setContents(html);
-//        Assert.assertEquals(expected, entry.getContents());
-//    }
-//    
-//    @Test
-//    public void test_setContents_likes_to_trim_whitespace() {
-//        String html
-//            = "  this <img> tag and <img/> \n and \n <img /> and <img></img>  \n\n  ";
-//        
-//        String expected 
-//            = "this <img> tag and <img/> \n and \n <img /> and <img></img>";
-//
-//        RssEntry entry = new RssEntry();
-//        entry.setContents(html);
-//        
-//    }
+    @Test
+    public void test_setContents_removes_hieght_and_width() {
+        String html 
+            = "<p><a href=\"https://mars.nasa.gov/news/9549/\">\n" +
+              "<img src=\"https://mars.nasa.gov/system/news_items/main_images/9549_PIA24925_MAIN.jpg\" \n   style=\"padding-right:10px; \n padding-bottom:5px;\" \n align=\"left\" \n alt=\"Read article: Team Assessing SHERLOC Instrument on NASA's Perseverance Rover\" \n width=\"100\" \n height=\"75\" \n border=\"0\" /></a><br /><img src='' height='5' foo='bar' width=9></img><br />Engineers are working to stabilize a dust cover on one of the science instrument’s cameras.</p><img src=\"f\"   width=''/><br clear=\"all\"/><img src='b' height=5></img><br /><img <";
+
+        String expected 
+            = "<p><a href=\"https://mars.nasa.gov/news/9549/\">\n" +
+              "<img style=\"width:99%; max-width:99%;\"  src=\"https://mars.nasa.gov/system/news_items/main_images/9549_PIA24925_MAIN.jpg\" \n   style=\"padding-right:10px; \n padding-bottom:5px;\" \n align=\"left\" \n alt=\"Read article: Team Assessing SHERLOC Instrument on NASA's Perseverance Rover\" \n resistewidth=\"100\" \n resisteheight=\"75\" \n border=\"0\" /></a><br /><img style=\"width:99%; max-width:99%;\"  src='' resisteheight='5' foo='bar' resistewidth=9></img><br />Engineers are working to stabilize a dust cover on one of the science instrument’s cameras.</p><img style=\"width:99%; max-width:99%;\"  src=\"f\"   resistewidth=''/><br clear=\"all\"/><img style=\"width:99%; max-width:99%;\"  src='b' resisteheight=5></img><br /><img style=\"width:99%; max-width:99%;\"  <";
+        
+        RssEntry entry = new RssEntry(); 
+        
+        entry.setContents(html);
+        System.out.printf("*1* %s%n", expected);
+        System.out.printf("*2* %s%n", entry.getContents());
+        Assert.assertEquals(expected, entry.getContents());
+    }
+
+    @Test
+    public void test_setContents_no_html() {
+        String html, expected;
+        html = expected =
+            "plain text content with no html";
+
+        RssEntry entry = new RssEntry();
+        entry.setContents(html);
+        Assert.assertEquals(expected, entry.getContents());
+    }
+    
+    @Test
+    public void test_setContents_lots_of_lessthan_but_no_tags() {
+        String html, expected;
+        html = expected =
+            "this < is < some<thin< that migh> be <<< difficult> to parse<?> ><?";
+
+        RssEntry entry = new RssEntry();
+        entry.setContents(html);
+        Assert.assertEquals(expected, entry.getContents());
+    }
+    
+    @Test
+    public void test_setContents_lots_of_img_tags_but_no_attributes() {        
+        String html = 
+            "this <img> tag and <img/> and <img /> and <img></img>";
+        String expected =
+            "this <img style=\"width:99%; max-width:99%;\" > tag and <img style=\"width:99%; max-width:99%;\" /> and <img style=\"width:99%; max-width:99%;\"  /> and <img style=\"width:99%; max-width:99%;\" ></img>";   
+        
+        RssEntry entry = new RssEntry();
+        entry.setContents(html);
+        System.out.printf("*A* %s%n", expected);
+        System.out.printf("*B* %s%n", entry.getContents());
+        Assert.assertEquals(expected, entry.getContents());
+    }
+    
+    @Test
+    public void test_setContents_likes_to_trim_whitespace() {
+        String html
+            = "  this <img> tag and <img/> \n and \n <img /> and <img></img>  \n\n  ";
+        
+        String expected 
+            = "this <img style=\"width:99%; max-width:99%;\"  style=\"width:99%; max-width:99%;\" > tag and <img style=\"width:99%; max-width:99%;\" /> \n and \n <img style=\"width:99%; max-width:99%;\"  /> and <img style=\"width:99%; max-width:99%;\" ></img>";
+
+        RssEntry entry = new RssEntry();
+        entry.setContents(html);
+        
+    }
     
      @Test
      public void testReplaceAllLowerCase() {
