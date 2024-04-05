d9b361162a8603adae918edd4c53403586c8242d
Michael Remijan <mjremijan@yahoo.com>
2024-04-05 14:10:31 -0500

    RESISTE-109 Completed:   org.jboss.weld.se:weld-se-core ............ 3.0.3.Final -> 3.1.9.Final
    This is a small step to get to version 6

--- ferris-resiste-console/src/main/java/module-info.java
+++ ferris-resiste-console/src/main/java/module-info.java
@@ -3,9 +3,9 @@
  * @author Michael Remijan mjremijan@yahoo.com @mjremijan
  */
 module org.ferris.resiste.console {
-    requires cdi.api;
-    requires javax.inject;
-    requires javax.annotation.api;
+    requires jakarta.enterprise.cdi.api;
+    requires jakarta.inject.api;    
+    requires java.annotation;
     requires java.sql;
     requires org.slf4j;
     requires ch.qos.logback.core;
